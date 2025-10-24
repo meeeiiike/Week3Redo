dd57fa65f7d723337d3997080fe2117aceeb610e
meike <g00407095@atu.ie>
2025-10-24 21:46:24 +0100

    init commit

---
+++ src/main/java/ie/atu/week3redo/Week3RedoApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3redo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3RedoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3RedoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week3redo/Week3RedoApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3redo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3RedoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
